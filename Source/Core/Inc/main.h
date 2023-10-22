6aee7c7c959c87b642a75b430508a848401360c7
HCrystalH <hvvh190102@gmail.com>
2023-10-22 16:41:01 +0700

    Revert "STM32 Configuration"
    This reverts commit 89eba46bc206d49dcd0a16f1d1db845c09a00f7d.

--- Source/Core/Inc/main.h
+++ Source/Core/Inc/main.h
@@ -58,54 +58,24 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define RED_VERTICAL_Pin GPIO_PIN_1
-#define RED_VERTICAL_GPIO_Port GPIOA
-#define YELLOW_VERTICAL_Pin GPIO_PIN_2
-#define YELLOW_VERTICAL_GPIO_Port GPIOA
-#define GREEN_VERTICAL_Pin GPIO_PIN_3
-#define GREEN_VERTICAL_GPIO_Port GPIOA
-#define RED_HORIZONTAL_Pin GPIO_PIN_4
-#define RED_HORIZONTAL_GPIO_Port GPIOA
-#define YELLOW_HORIZONTAL_Pin GPIO_PIN_5
-#define YELLOW_HORIZONTAL_GPIO_Port GPIOA
-#define GREEN_HORIZONTAL_Pin GPIO_PIN_6
-#define GREEN_HORIZONTAL_GPIO_Port GPIOA
-#define EN_VER1_Pin GPIO_PIN_7
-#define EN_VER1_GPIO_Port GPIOA
-#define BUTTON1_Pin GPIO_PIN_0
-#define BUTTON1_GPIO_Port GPIOB
-#define BUTTON2_Pin GPIO_PIN_1
-#define BUTTON2_GPIO_Port GPIOB
-#define BUTTON3_Pin GPIO_PIN_2
-#define BUTTON3_GPIO_Port GPIOB
-#define EN_7SEG_VER1_Pin GPIO_PIN_11
-#define EN_7SEG_VER1_GPIO_Port GPIOB
-#define EN_7SEG_VER2_Pin GPIO_PIN_12
-#define EN_7SEG_VER2_GPIO_Port GPIOB
-#define EN_7SEG_HORI1_Pin GPIO_PIN_13
-#define EN_7SEG_HORI1_GPIO_Port GPIOB
-#define EN_7SEG_HORI2_Pin GPIO_PIN_14
-#define EN_7SEG_HORI2_GPIO_Port GPIOB
-#define EN_VER2_Pin GPIO_PIN_8
-#define EN_VER2_GPIO_Port GPIOA
-#define EN_HORI1_Pin GPIO_PIN_9
-#define EN_HORI1_GPIO_Port GPIOA
-#define EN_HORI2_Pin GPIO_PIN_10
-#define EN_HORI2_GPIO_Port GPIOA
-#define seg_a_Pin GPIO_PIN_3
-#define seg_a_GPIO_Port GPIOB
-#define seg_b_Pin GPIO_PIN_4
-#define seg_b_GPIO_Port GPIOB
-#define seg_c_Pin GPIO_PIN_5
-#define seg_c_GPIO_Port GPIOB
-#define seg_d_Pin GPIO_PIN_6
-#define seg_d_GPIO_Port GPIOB
-#define seg_e_Pin GPIO_PIN_7
-#define seg_e_GPIO_Port GPIOB
-#define seg_f_Pin GPIO_PIN_8
-#define seg_f_GPIO_Port GPIOB
-#define seg_g_Pin GPIO_PIN_9
-#define seg_g_GPIO_Port GPIOB
+#define PA0_Pin GPIO_PIN_0
+#define PA0_GPIO_Port GPIOA
+#define PA1_Pin GPIO_PIN_1
+#define PA1_GPIO_Port GPIOA
+#define PA2_Pin GPIO_PIN_2
+#define PA2_GPIO_Port GPIOA
+#define PA3_Pin GPIO_PIN_3
+#define PA3_GPIO_Port GPIOA
+#define PA4_Pin GPIO_PIN_4
+#define PA4_GPIO_Port GPIOA
+#define PA5_Pin GPIO_PIN_5
+#define PA5_GPIO_Port GPIOA
+#define PA6_Pin GPIO_PIN_6
+#define PA6_GPIO_Port GPIOA
+#define PA7_Pin GPIO_PIN_7
+#define PA7_GPIO_Port GPIOA
+#define Button1_Pin GPIO_PIN_0
+#define Button1_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
